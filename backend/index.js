1b7065ee5b1212a02cc8eafec5908ed9f1b077de
Akande Lateef <akandelateef0@gmail.com>
2025-03-05 05:21:18 +0100

    fix: router

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 9000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/send-email', router);
+// app.use('/send-email', router);
 app.use('/subscribe', subscribeRoute);
 
 connectDb();
